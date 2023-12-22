ca92ecd9fbe97f1226db3cdbeb21bc9dd51259e4
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-12-22 16:09:41 +0800

    Create enum for available tables

---
+++ src/app/shared/issue-tables/available-tables.enum.ts
@@ -0,0 +1,12 @@
+export enum PhraseTeamResponseTable {
+  Pending = 'pending',
+  Responded = 'responded',
+  Faulty = 'faulty'
+}
+
+export enum PhraseTesterResponseTable {
+  Pending = 'pending',
+  Responded = 'responded',
+  Faulty = 'faulty',
+  Accepted = 'accepted'
+}
